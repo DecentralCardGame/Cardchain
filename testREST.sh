cad8efb77bf3ea43bb045bf6ddc18e375174ea4f
Patrick Wieth <patrick.wieth@googlemail.com>
2019-02-22 22:17:25 +0100

    rest server tests via bash working

--- testREST.sh
+++ testREST.sh
@@ -1,35 +1,108 @@
 #!/bin/sh
 
-ALICEKEY=$(nscli keys show alice --address)
-URL="https://localhost:1317/auth/accounts/${ALICEKEY}"
-echo "REQ URL: ${URL}"
+colorMagenta='\033[0;35m'
+colorCyan='\033[0;36m'
+colorGreen='\033[0;32m'
+colorRed='\033[0;31m'
+colorNormal='\033[0m'
 
-nohup nscli rest-server --chain-id testCardchain --trust-node &
+JSON=$(cat tests/testREST.json)
+
+# iterate over all prevaluations
+printf "${colorCyan}executing prevaluation commands ${colorNormal}\n"
+
+length=$(echo ${JSON} | jq -r '.prevaluate' | jq length)
+
+for i in `seq 0 $((length-1))`;
+  do
+    cmd=$(echo ${JSON} | jq -r ".prevaluate["${i}"]")
+    printf "${colorMagenta}${cmd}${colorNormal} \n"
+    eval $cmd
+  done
+
+
+URL="https://localhost:1317/auth/accounts/"
+printf "${colorCyan}querying rest-server ${colorNormal}"
 
 while [ -z "$RES" ]; do
   sleep 1
-  printf 'querying rest-server'
-  RES=$(curl -s --insecure ${URL});
+
+  RES=$(curl -s --insecure ${URL}${AliceAddress});
+  RES2=$(curl -s --insecure ${URL}${BobAddress});
 
   printf '.'
 done
 
 printf '\n'
 
-SEQUENCE=$(echo $RES | jq -r  '.value.sequence')
-ACCNUM=$(echo $RES | jq -r  '.value.account_number')
+AliceSequence=$(echo $RES | jq -r  '.value.sequence')
+AliceAccountNumber=$(echo $RES | jq -r  '.value.account_number')
+BobSequence=$(echo $RES2 | jq -r  '.value.sequence')
+BobAccountNumber=$(echo $RES2 | jq -r  '.value.account_number')
 
-echo $SEQUENCE
-echo $ACCNUM
+#echo $AliceSequence" - "$AliceAccountNumber" - "$BobSequence" - "$BobAccountNumber
 
-REQ='{"base_req":{"name":"alice","from":"alice","password":"asdfasdf","chain_id":"testCardchain","sequence":"'
-REQ+=$SEQUENCE
-REQ+='","account_number":"'
-REQ+=$ACCNUM
-REQ+='"},"amount":"990credits","buyer":"cosmos1grwugureenk22gft5nmdtux2a3h5uf0xa64kdk"}'
+# iterate over all TestCases
+length=$(echo ${JSON} | jq -r '.TestCases' | jq length)
 
-RES=$(curl -XPOST -s --insecure https://localhost:1317/cardservice/buy_card_scheme --data-binary $REQ)
+for i in `seq 0 $((length-1))`;
+  do
+    # get the ith case
+    currentCase=$(echo ${JSON} | jq -r ".TestCases["${i}"]")
 
-echo $RES
+    printf "${colorCyan}running test: "$(echo ${currentCase} | jq -r ".name")"${colorNormal}\n"
 
-kill %1
+    # evaluate all .evaluate entries
+    length=$(echo ${currentCase} | jq -r '.evaluate' | jq length)
+
+    exp1='.evaluate['
+    exp2=']'
+
+    for i in `seq 0 $((length-1))`;
+      do
+        cmd[${i}]=$(echo ${currentCase} | jq -r "$exp1$i$exp2")
+        printf "${colorMagenta}${cmd[${i}]}${colorNormal}\n"
+        result[${i}]=$(eval ${cmd[${i}]})
+      done
+
+    # fill in %s entries
+    currJSON=$(printf "${currentCase}" "${result[@]}")
+
+    REQ=$(echo ${currJSON} | jq -c '.reqBody')
+    Route=$(echo ${currJSON} | jq -r '.route')
+
+    #echo $REQ
+    #echo $Route
+
+    cmd="curl -XPOST -s --insecure ${Route} --data-binary ${REQ}"
+    echo $cmd
+
+    RES=$($cmd)
+
+    echo $RES
+
+    #compare with expected
+    cmd="json validate --document-json='${RES}' --schema-json='$(echo ${currJSON} | jq -r '.expected')'"
+    echo $cmd
+
+    echo -e "${colorRed}"
+    eval $cmd
+    if [ "$?" = "0" ]; then
+    	echo -e "${colorGreen}Test passed"
+    else
+    	echo -e "${?}"
+    fi
+    echo -e "${colorNormal}"
+
+  done
+
+# iterate over all postvaluations
+printf "${colorCyan}executing postvaluation commands ${colorNormal}\n"
+length=$(echo ${JSON} | jq -r '.postvaluate' | jq length)
+
+for i in `seq 0 $((length-1))`;
+  do
+    cmd=$(echo ${JSON} | jq -r ".postvaluate["${i}"]")
+    echo -e "${colorMagenta}${cmd}${colorNormal}"
+    eval $cmd
+  done
