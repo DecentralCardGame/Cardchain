b713c20bb32d4793b8322c404098f2a4e3462b81
patrickwieth <patrick.wieth@googlemail.com>
2019-10-27 22:53:33 +0100

    setup.sh script writes out keys into a logfile

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ rm ~/.csd/ -r
 csd init --chain-id testCardchain
 printf 'asdfasdf\n' | cscli keys delete alice
 printf 'asdfasdf\n' | cscli keys delete bob
-printf 'asdfasdf\nasdfasdf\n' | cscli keys add alice
-printf 'asdfasdf\nasdfasdf\n' | cscli keys add bob
+printf 'asdfasdf\nasdfasdf\n' | cscli keys add alice &>> keysAliceBob.log
+printf 'asdfasdf\nasdfasdf\n' | cscli keys add bob &>> keysAliceBob.log
 csd add-genesis-account $(cscli keys show alice --address) 1000mycoin,1000credits
 csd add-genesis-account $(cscli keys show bob --address) 1000mycoin,1000credits
