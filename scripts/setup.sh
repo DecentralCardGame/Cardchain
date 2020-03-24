f60f2470797c649e016a65a42be58ed7458a5825
Patrick Wieth <patrick.wieth@gmail.com>
2020-03-24 17:21:49 +0100

    change create_empty_blocks to false

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,3 +9,5 @@ printf 'asdfasdf\nasdfasdf\n' | cscli keys add bob &>> keysAliceBob.log
 csd add-genesis-account $(cscli keys show alice --address) 1000stake,1000credits
 csd add-genesis-account $(cscli keys show bob --address) 1000stake,1000credits
 csd add-genesis-account cosmos178x4cwg7zuppfgypdd7c0wy0kp304wad9v0awe 1stake,1credits
+
+sed -i 's/create_empty_blocks = true/create_empty_blocks = false/g' ~/.csd/config/config.toml
