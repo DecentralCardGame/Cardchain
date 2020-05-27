eae52c5f1d900310b04623f34ed01d993cb08ce3
Patrick Wieth <patrick.wieth@googlemail.com>
2020-05-27 13:39:08 +0200

    adjust order of csd start and csd export in docker-run

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-csd export --for-zero-height > /var/blockchain/genesis.json
 /go/bin/csd start --rpc.laddr tcp://0.0.0.0:26657
+csd export --for-zero-height > /var/blockchain/genesis.json
