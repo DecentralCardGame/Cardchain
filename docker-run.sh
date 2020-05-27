b15dece31044ee2fb5cc3b05218d49941884bb85
Patrick Wieth <patrick.wieth@gmail.com>
2020-05-27 14:46:34 +0200

    add script to use genesis.json for zero height

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if test -f "/var/blockchain/genesis.json"; then
+  /go/bin/csd unsafe-reset-all
+  mv /var/blockchain/genesis.json ~/.csd/config/
+fi
+
 /go/bin/csd start --rpc.laddr tcp://0.0.0.0:26657
 csd export --for-zero-height > /var/blockchain/genesis.json
