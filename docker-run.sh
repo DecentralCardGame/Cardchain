a44272f1b87b4f98f8cc7f236df4652a145e5a89
Patrick Wieth <patrick.wieth@gmail.com>
2020-06-04 15:24:46 +0200

    add more logging and backup to docker-run script

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if test -f "/var/blockchain/genesis.json"; then
+  echo -e "'\033[0;31m' genesis.json found - reloading from there'\033[0m'"
   /go/bin/csd unsafe-reset-all
+  cp /var/blockchain/genesis.json /var/blockchain/genesis.json.backup
   mv /var/blockchain/genesis.json ~/.csd/config/
 fi
 
