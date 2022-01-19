5f27b75bad4c46ed1b1df252cde6c7875adc3a77
lxgr-linux <lxgr@protonmail.com>
2022-01-19 20:21:25 +0100

    Added keeper.InitUser and keeper.GetUser

--- x/cardchain/keeper/keeper.go
+++ x/cardchain/keeper/keeper.go
@@ -51,11 +51,35 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 
 func (k Keeper) Createuser(ctx sdk.Context, newUser sdk.AccAddress, alias string) types.User {
 	// check if user already exists
-	store := ctx.KVStore(k.UsersStoreKey)
+	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(newUser)
 
 	if bz == nil {
 		k.InitUser(ctx, newUser, alias)
 	}
 	return k.GetUser(ctx, newUser)
+}
+
+func (k Keeper) InitUser(ctx sdk.Context, address sdk.AccAddress, alias string) {
+	store := ctx.KVStore(k.storeKey)
+
+	if alias == "" {
+		alias = "newbie"
+	}
+	newUser := types.NewUser()
+	newUser.Alias = alias
+	k.CoinKeeper.AddCoins(ctx, address, sdk.Coins{sdk.NewInt64Coin("credits", 10000)})
+	const votingRightsExpirationTime = 86000
+	newUser.VoteRights = k.GetVoteRightToAllCards(ctx, ctx.BlockHeight()+votingRightsExpirationTime)		// TODO this might be a good thing to remove later, so that sybil voting is not possible
+
+	store.Set(address, k.cdc.MustMarshalBinaryBare(newUser))
+}
+
+func (k Keeper) GetUser(ctx sdk.Context, address sdk.AccAddress) types.User {
+	store := ctx.KVStore(k.storeKey)
+	bz := store.Get(address)
+
+	var gottenUser types.User
+	k.cdc.MustUnmarshalBinaryBare(bz, &gottenUser)
+	return gottenUser
 }
