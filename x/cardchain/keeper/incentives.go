f0888f0dd063951baedd425f5593f3137b243c17
lxgr-linux <lxgr@protonmail.com>
2022-03-02 14:16:27 +0100

    added comments for incentives.go

--- x/cardchain/keeper/incentives.go
+++ x/cardchain/keeper/incentives.go
@@ -10,6 +10,7 @@ const (
 	Votes24ValueKey = "votes/24h"
 )
 
+// GetWinnerIncentives calculates incentives for the winners pool
 func (k Keeper) GetWinnerIncentives(ctx sdk.Context) float32 {
 	games := float32(k.GetGames(ctx))
 	votes := float32(k.GetVotes(ctx))
@@ -17,6 +18,7 @@ func (k Keeper) GetWinnerIncentives(ctx sdk.Context) float32 {
 	return games / (votes*gVR + games)
 }
 
+// GetBalancerIncentives calculates incentives for the ballancers pool
 func (k Keeper) GetBalancerIncentives(ctx sdk.Context) float32 {
 	games := float32(k.GetGames(ctx))
 	votes := float32(k.GetVotes(ctx))
@@ -24,6 +26,7 @@ func (k Keeper) GetBalancerIncentives(ctx sdk.Context) float32 {
 	return (votes * gVR) / (votes*gVR + games)
 }
 
+// GetGames Gets the number of games played in the last 24 hours
 func (k Keeper) GetGames(ctx sdk.Context) (num int64) {
 	games := k.GetRunningAverage(ctx, Games24ValueKey)
   for _, val := range games.Arr {
@@ -35,6 +38,7 @@ func (k Keeper) GetGames(ctx sdk.Context) (num int64) {
   return
 }
 
+// GetVotes Gets the number of votes made in the last 24 hours
 func (k Keeper) GetVotes(ctx sdk.Context) (num int64) {
 	votes := k.GetRunningAverage(ctx, Votes24ValueKey)
   for _, val := range votes.Arr {
