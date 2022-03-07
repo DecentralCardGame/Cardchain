c6d18f873be419a7176e1312d776849353a4f42c
lxgr-linux <lxgr@protonmail.com>
2022-03-07 22:04:20 +0100

    added test for collection.go

--- x/cardchain/keeper/card_test.go
+++ x/cardchain/keeper/card_test.go
@@ -5,19 +5,27 @@ import (
 
 	testkeeper "github.com/DecentralCardGame/Cardchain/testutil/keeper"
 	"github.com/DecentralCardGame/Cardchain/x/cardchain/types"
+	"github.com/DecentralCardGame/Cardchain/x/cardchain/keeper"
   sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCard(t *testing.T) {
-	k, ctx := testkeeper.CardchainKeeper(t)
-  addr, err := sdk.AccAddressFromBech32("cosmos15kq043zhu0wjyuw9av0auft06y3v2kxss862qf")
+// Sets up a Card
+func setUpCard(ctx sdk.Context, k *keeper.Keeper) types.Card {
+	addr, err := sdk.AccAddressFromBech32("cosmos15kq043zhu0wjyuw9av0auft06y3v2kxss862qf")
   if err != nil {
   	panic(err)
   }
 	card := types.NewCard(addr)
+	card.Artist = addr.String()
 
 	k.SetCard(ctx, 0, card)
+	return card
+}
+
+func TestCard(t *testing.T) {
+	k, ctx := testkeeper.CardchainKeeper(t)
+  card := setUpCard(ctx, k)
 
 	require.EqualValues(t, card, k.GetCard(ctx, 0))
   require.EqualValues(t, []*types.Card{&card}, k.GetAllCards(ctx))
