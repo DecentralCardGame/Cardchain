a0a44302abded7762ccb27f865158b3ea64adeea
lxgr-linux <lxgr@protonmail.com>
2022-04-18 02:06:05 +0200

    extensions to setup

--- scripts/test_setup.sh
+++ scripts/test_setup.sh
@@ -10,6 +10,6 @@ Cardchaind tx cardchain createuser $(Cardchaind keys show "Cooler Artist" --addr
 
 Cardchaind tx cardchain buy-card-scheme 8000000000ucredits --from "Cooler Typ"
 
-Cardchaind tx cardchain save-card-content 0 '{"Entity":{"CardName":"Name","CastingCost":13,"Class":{"Nature":false,"Mysticism":false,"Technology":true,"Culture":true},"Abilities":[{"Pay":{"ManaAmount":2,"Effects":[{"Arm":{"Target":"RANDOM","Amount":{"SimpleIntValue":3}}}]}}],"Attack":10,"Health":10,"FlavourText":"-.-","Tags":["SPIRIT"],"Keywords":["["ARM","PAY"]"],"RulesTexts":["This could be your RulesText",""]}}' "no notes" "$(Cardchaind keys show "Cooler Artist" --address)" --from "Cooler Typ"
+Cardchaind tx cardchain save-card-content 0 '{"Entity":{"CardName":"Yees","CastingCost":13,"Class":{"Nature":false,"Mysticism":false,"Technology":true,"Culture":true},"Abilities":[{"Pay":{"ManaAmount":2,"Effects":[{"Arm":{"Target":"RANDOM","Amount":{"SimpleIntValue":3}}}]}}],"Attack":10,"Health":10,"FlavourText":"-.-","Tags":["SPIRIT"],"Keywords":["[\"Periodic\", \"Ravage\"]"],"RulesTexts":["This could be your RulesText",""]}}' "no notes" "$(Cardchaind keys show "Cooler Artist" --address)" --from "Cooler Typ" --gas 99000000
 
-Cardchaind tx cardchain add-artwork 0 "data:image/jpeg;base64,$(base64 ./pic.jpg)" true --from "Cooler Artist"
+Cardchaind tx cardchain add-artwork 0 "data:image/jpeg;base64,$(base64 ./pic.jpg)" true --from "Cooler Artist" --gas 990000000
