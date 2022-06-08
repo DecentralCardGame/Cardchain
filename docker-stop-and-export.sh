92f757c9e8a87b681f1ea3630407ab8b085a2dc6
Patrick Wieth <patrick.wieth@googlemail.com>
2022-06-08 20:41:31 +0200

    docker-stop-and-export now automatically writes backup files

--- docker-stop-and-export.sh
+++ docker-stop-and-export.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+PID=$(docker-compose exec blockchain pidof Cardchaind)
+echo PID:$PID
+
 docker-compose exec blockchain pkill Cardchaind
+
+while $(docker-compose exec blockchain pkill -0 Cardchaind); do
+	sleep 1
+done 
+
+echo "writing backup"
+
+sh ./scripts/backupGenesis.sh
