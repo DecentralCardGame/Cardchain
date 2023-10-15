aae928bc01dc3b6796a6a70c07ab41cc9f8b3660
lxgr-linux <lxgr@protonmail.com>
2023-10-15 23:46:56 +0200

    Fixed openmatch args

--- x/cardchain/client/cli/tx_msg_open_match.go
+++ x/cardchain/client/cli/tx_msg_open_match.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"encoding/json"
 	"strconv"
 
 	"github.com/DecentralCardGame/Cardchain/x/cardchain/types"
@@ -14,9 +15,9 @@ var _ = strconv.Itoa(0)
 
 func CmdMsgOpenMatch() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "open-match",
+		Use:   "open-match [player-a] [player-b] [player-a-deck] [player-b-deck]",
 		Short: "Broadcast message OpenMatch",
-		Args:  cobra.ExactArgs(0),
+		Args:  cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 
 			clientCtx, err := client.GetClientTxContext(cmd)
@@ -24,8 +25,23 @@ func CmdMsgOpenMatch() *cobra.Command {
 				return err
 			}
 
+			var argPlayerADeck []uint64
+			var argPlayerBDeck []uint64
+			err = json.Unmarshal([]byte(args[2]), &argPlayerADeck)
+			if err != nil {
+				return err
+			}
+			err = json.Unmarshal([]byte(args[3]), &argPlayerBDeck)
+			if err != nil {
+				return err
+			}
+
 			msg := types.NewMsgOpenMatch(
 				clientCtx.GetFromAddress().String(),
+				args[0],
+				args[1],
+				argPlayerADeck,
+				argPlayerBDeck,
 			)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
