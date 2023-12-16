66c2200abce9061357c31c2152653da7b99ee384
Patrick Wieth <patrick.wieth@googlemail.com>
2023-12-16 14:08:08 +0100

    update scripts

--- scripts/paramchange/submitproposal.sh
+++ scripts/paramchange/submitproposal.sh
@@ -4,3 +4,5 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 Cardchaind tx gov submit-legacy-proposal param-change paramchange.json --from jannik
+
+Cardchaind q gov proposals
