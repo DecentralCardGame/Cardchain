96f919a3744005567606567c4d4e2b75ac263004
Patrick Wieth <patrick.wieth@googlemail.com>
2024-05-23 12:47:31 +0200

    nodejs discord server wors

--- goat/app.js
+++ goat/app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 31337;
 
 //let codeValue = 'oHISzAR5RdTef7jVntWuA48Gf044Vr'
 
-const fetchUser = async code => {
+const fetchUser = code => {
 	let query = querystring.stringify({
 		'client_id': '1242405621815316502',
 		'client_secret': 'SdY9h2ilQb42AKV3dL8pscd9vcvUc0Bo',
@@ -20,28 +20,35 @@ const fetchUser = async code => {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
 
-	const token = await axios.post('https://discordapp.com/api/oauth2/token', query, headers)
+	return axios.post('https://discordapp.com/api/oauth2/token', query, headers)
+	.then(token => {
+		return axios.get(`https://discordapp.com/api/users/@me`, {
+	        headers: {
+	            "Authorization": "Bearer " + token.data.access_token,
+	    	}
+	    })
+	})
 
-	console.log("token", token.status)
-
-    const user = await axios.get(`https://discordapp.com/api/users/@me`, {
-        headers: {
-            "Authorization": "Bearer " + token.data.access_token,
-    	}
-    })
-
-	console.log("user", user.status)
-	return await user
 }
 
 app.get('/', (req, res) => {
+	if (!req.query.code) {
+		throw new Error('No code provided - you must provide a token code from Discord')
+	}
+
 	console.log("code", req.query.code)
-	fetchUser(req.query.code)
-		.then(user => {
-			console.log("response", user)
-			console.log(user.status, user.statusText);
-		 	res.send(user.data);
-		});
+
+	return fetchUser(req.query.code)
+	.then(user => {
+		console.log("response", user.data)
+		console.log("status", user.status, user.statusText);
+	 	res.send(user.data);
+	})
+	.catch(err => {
+		console.error(err.response)
+		res.status(500).send(err)
+	})
+		
 });
 
 app.listen(PORT, () => {
