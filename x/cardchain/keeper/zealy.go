41c27c1a8a83dec14faedf453d571ac17ae2456e
lxgr-linux <lxgr@protonmail.com>
2024-06-02 13:33:25 +0200

    feat(#224): Added zealy query

--- x/cardchain/keeper/zealy.go
+++ x/cardchain/keeper/zealy.go
@@ -7,32 +7,32 @@ import (
     sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// SetZealy Sets a zealy id and 
+// SetZealy Sets a zealy id
 func (k Keeper) SetZealy(ctx sdk.Context, zealyId string, zealy types.Zealy) {
     store := ctx.KVStore(k.zealyStoreKey)
     store.Set([]byte(zealyId), k.cdc.MustMarshal(&zealy))
 }
 
-// GetZealy Gets a user from store
+// GetZealy Gets a zealy from store
 func (k Keeper) GetZealy(ctx sdk.Context, zealyId string) (zealy types.Zealy, err error) {
     store := ctx.KVStore(k.zealyStoreKey)
     bz := store.Get([]byte(zealyId))
 
     if bz == nil {
-        err = fmt.Errorf("zealyId '%s' not in store", zealyId)
+        err = fmt.Errorf("zealyId `%s` not in store", zealyId)
     } else {
         k.cdc.MustUnmarshal(bz, &zealy)
     }
     return
 }
 
-// GetZealyIterator Returns an iterator for all users
+// GetZealyIterator Returns an iterator for all zealys
 func (k Keeper) GetZealyIterator(ctx sdk.Context) sdk.Iterator {
     store := ctx.KVStore(k.zealyStoreKey)
     return sdk.KVStorePrefixIterator(store, nil)
 }
 
-// GetAllZealys Gets all users from store
+// GetAllZealys Gets all zealys from store
 func (k Keeper) GetAllZealys(ctx sdk.Context) (allZealys []*types.Zealy, allZealyIds []string) {
     iterator := k.GetZealyIterator(ctx)
     for ; iterator.Valid(); iterator.Next() {
--- x/cardchain/types/query.pb.gw.go
+++ x/cardchain/types/query.pb.gw.go
@@ -877,6 +877,60 @@ func local_request_Query_QCardContents_0(ctx context.Context, marshaler runtime.
 
 }
 
+func request_Query_QAccountFromZealy_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryQAccountFromZealyRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["zealyId"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "zealyId")
+	}
+
+	protoReq.ZealyId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "zealyId", err)
+	}
+
+	msg, err := client.QAccountFromZealy(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Query_QAccountFromZealy_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryQAccountFromZealyRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["zealyId"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "zealyId")
+	}
+
+	protoReq.ZealyId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "zealyId", err)
+	}
+
+	msg, err := server.QAccountFromZealy(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterQueryHandlerServer registers the http handlers for service Query to "mux".
 // UnaryRPC     :call QueryServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -1274,6 +1328,29 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 
 	})
 
+	mux.Handle("GET", pattern_Query_QAccountFromZealy_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Query_QAccountFromZealy_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Query_QAccountFromZealy_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1655,6 +1732,26 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_Query_QAccountFromZealy_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Query_QAccountFromZealy_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Query_QAccountFromZealy_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1692,6 +1789,8 @@ var (
 	pattern_Query_RarityDistribution_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"DecentralCardGame", "Cardchain", "cardchain", "rarity_distribution", "setId"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Query_QCardContents_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"DecentralCardGame", "Cardchain", "cardchain", "q_card_contents", "cardIds"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_Query_QAccountFromZealy_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"DecentralCardGame", "Cardchain", "cardchain", "q_account_from_zealy", "zealyId"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -1728,4 +1827,6 @@ var (
 	forward_Query_RarityDistribution_0 = runtime.ForwardResponseMessage
 
 	forward_Query_QCardContents_0 = runtime.ForwardResponseMessage
+
+	forward_Query_QAccountFromZealy_0 = runtime.ForwardResponseMessage
 )
