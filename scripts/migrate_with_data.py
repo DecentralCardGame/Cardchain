b7bc693ae972763904a1fcfe11f7f3b5311677a5
Patrick Wieth <patrick.wieth@googlemail.com>
2024-06-23 21:00:29 +0200

    update migrate script for zealy stuff

--- scripts/migrate_with_data.py
+++ scripts/migrate_with_data.py
@@ -12,12 +12,25 @@ assert len(args) == 3, f"Error: Syntax: {args[0]} [old_genesis] [new_genesis]"
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 gameserver_addr = ["cc1z94z55n2rr4rmjf4ea0m7ykgh9a8urwzrlsxt4", "cc1ch66e3f0szxy8q976rsq5y07esmgdqzj70dfpu"]
-early_access_addr = ["cc14km80077s0hch3sh38wh2hfk7kxfau4456r3ej", "cc1tmhtms6ahkrxltx3hkmmf2dteqj4pv0thwhdxa"]
 alpha_creator = "cc14km80077s0hch3sh38wh2hfk7kxfau4456r3ej"
 del_cards = []  # [370, 346, 258]
 
 file_path_old = args[1]
 file_path_new = args[2]
+
+
+# early access, boosterpacks, airdrop from zealy
+
+airdrop_accs = []
+boosterpack_accs = []
+early_access_addr = ["cc14km80077s0hch3sh38wh2hfk7kxfau4456r3ej", "cc1tmhtms6ahkrxltx3hkmmf2dteqj4pv0thwhdxa"]
+with open(os.path.join(__location__, "./zealy.tsv"), "r", encoding="utf8") as zealy_file:
+    tsv_reader = csv.DictReader(zealy_file, delimiter="\t")
+    for entry in tsv_reader:
+        airdrop_accs.append((entry["CCAddress"], entry["Airdrop"]))
+        boosterpack_accs.append([entry["CCAddress"], entry["BoosterPacks"]])
+        if entry["EarlyAccessToGame"] == "TRUE":
+            early_access_addr.append(entry["CCAddress"])
 
 genesisAccs = []
 # here we load the balances of addresses that start with balances on CC
@@ -81,12 +94,12 @@ for param in params:
         params[param] = old_dict["app_state"]["cardchain"]["params"][param]
 new_dict["app_state"]["cardchain"]["params"] = params
 
-# set balanceAnchor, for alpha creator true, for all others false
-for key in new_dict["app_state"]["cardchain"]["cardRecords"]:
-    if key["owner"] == alpha_creator:
-        key["balanceAnchor"] = True
-    else:
-        key["balanceAnchor"] = False
+# set balanceAnchor, for alpha creator true, for all others false, deactivated because already fine for most cards
+# for key in new_dict["app_state"]["cardchain"]["cardRecords"]:
+#     if key["owner"] == alpha_creator:
+#         key["balanceAnchor"] = True
+#     else:
+#         key["balanceAnchor"] = False
 
 for idx, addr in enumerate(old_dict["app_state"]["cardchain"]["addresses"]):
     # set reportmatches to true for gameserver addresses
@@ -100,17 +113,27 @@ for idx, addr in enumerate(old_dict["app_state"]["cardchain"]["addresses"]):
             new_dict["app_state"]["auth"]["accounts"].append(i)
             break
     # limit balances to 5k for all old accounts (genesis accs + alice and bob will have more)
-    for i in old_dict["app_state"]["bank"]["balances"]:
+    for i in old_dict["app_state"]["bank"]["balances"]:  
         if i["address"] == addr:
             for idx, coin in enumerate(i["coins"]):
                 # adjust BPFs
                 if coin["denom"] == "ubpf":
-                    # use flat value for all others (TODO ON LAUNCH THIS SHOULD BE 0)
-                    i["coins"][idx]["amount"] = "5000000"
+                    bpf = 0                    
                     # use real bpf value for genesisAddresses
                     for acc in genesisAccs:
                         if acc[0] == addr:
-                            i["coins"][idx]["amount"] = str(int(acc[1]) * 1000000)
+                            bpf += int(acc[1]) * 1000000
+                    # zealy airdrop
+                    for acc in airdrop_accs:
+                        if acc[0] == addr:
+                            bpf += int(acc[1]) * 1000000
+
+                    # use flat value for all others (TODO ON LAUNCH THIS SHOULD BE 0)
+                    if bpf < 5000000:
+                        bpf = 5000000
+
+                    i["coins"][idx]["amount"] = str(bpf)
+
                     # give bpf to alpha creator (jannik)
                     if addr == alpha_creator:
                         i["coins"][idx]["amount"] = "1000000000"
@@ -121,11 +144,18 @@ for idx, addr in enumerate(old_dict["app_state"]["cardchain"]["addresses"]):
             new_dict["app_state"]["bank"]["balances"].append(i)
             break
 
-# Remove deprecated voteRights from users
+# Remove deprecated voteRights from users and more shenanigans like booster packs and early access
 for addr, user in zip(new_dict["app_state"]["cardchain"]["addresses"], new_dict["app_state"]["cardchain"]["users"]):
+
     if "voteRights" in user:
         del user["voteRights"]
-    user["boosterPacks"] = [pack for pack in user["boosterPacks"] if pack["setId"] not in ["0", "2"]]  # turn of later
+
+    for entry in boosterpack_accs:
+        if entry[0] == addr:
+            num_packs = int(entry[1])
+            for x in range(num_packs):
+                user["boosterPacks"].append({'dropRatiosPerPack': ['150', '50', '1'], 'raritiesPerPack': ['4', '2', '1'], 'setId': '1', 'timeStamp': '0'})
+    
     user["earlyAccess"] = user.get(
         "earlyAccess",
         {"active": False, "invitedUser": "", "invitedByUser": ""}
