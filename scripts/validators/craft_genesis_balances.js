b4addc2c8afecf82343df0c1170e34db48cd186a
Patrick Wieth <patrick.wieth@googlemail.com>
2024-07-08 22:57:43 +0200

    fix craft genesis balances script

--- scripts/validators/craft_genesis_balances.js
+++ scripts/validators/craft_genesis_balances.js
@@ -9,19 +9,22 @@ let data = [];
 files.forEach(file => {
 	let content = fs.readFileSync('snapshots/'+file, "utf8");
 	let contentArray = R.map(R.split('\t'), R.split('\n', content))
-	data.push(contentArray)
+	data = R.append(contentArray, data)
 });
 
-//console.log(R.mergeAll(data))
+let dataObj = {}
+data.forEach(file => {
+	let fileObj = {}
+	file.forEach(entry => {
+		if (dataObj[entry[0]]) dataObj[entry[0]] += parseInt(entry[1])
+		else dataObj[entry[0]] = parseInt(entry[1])	
+	})
+})
 
-let saveableData = []
-R.forEachObjIndexed(entry => {
-		saveableData.push(entry)
-	},
-	R.mergeAll(data)
-)
-saveableData = R.join("\n", R.map(R.join("	"), saveableData))
+saveableData = []
+R.mapObjIndexed((val, key) => saveableData.push(key+"\t"+val), dataObj)
+saveableData = saveableData.join("\n")
 
 console.log(saveableData)
-fs.writeFileSync("genesis_balances.tsv", JSON.stringify(saveableData)); 
+fs.writeFileSync("merged_genesis_balances.tsv", saveableData); 
 
