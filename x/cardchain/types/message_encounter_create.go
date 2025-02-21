05d97f3798177cb186fa113ce617db6f3786d222
lxgr-linux <lxgr@protonmail.com>
2025-02-21 15:22:49 +0100

    fix(#245): Updated ecnounter drawlist length

--- x/cardchain/types/message_encounter_create.go
+++ x/cardchain/types/message_encounter_create.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	fmt "fmt"
+
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -23,5 +25,18 @@ func (msg *MsgEncounterCreate) ValidateBasic() error {
 	if err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
+
+	if len(msg.Drawlist) > 40 || len(msg.Drawlist) < 1 {
+		return errorsmod.Wrapf(ErrInvalidData, "invalid drawlist length, max 40 is '%d'", len(msg.Drawlist))
+	}
+
+	if msg.Name == "" {
+		return errorsmod.Wrap(ErrInvalidData, "encounter needs a name")
+	}
+
+	if len(msg.Image) > ArtworkMaxSize {
+		return errorsmod.Wrap(ErrImageSizeExceeded, fmt.Sprint(len(msg.Image)))
+	}
+
 	return nil
 }
